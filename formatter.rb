a5f812be5976935fd862b3cec80eae55698dfb68
Griffin Schneider <griffinschneider@gmail.com>
2013-11-19 22:26:12 -0500

    Duplicate objc-formatter.awk functionality, plus sorting property attributes and formatting class methods.

--- formatter.rb
+++ formatter.rb
@@ -3,11 +3,6 @@ require 'optparse'
 
 options = {}
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: format.rb [options] input output"
-  opts.separator "Note: With no options, format.rb formats all files with a filetype it understands that have staged git changes."
-  opts.on("-a", "--all", "Format or check all files under format.rb's jurisdiction.") {|a| options[:all] = a}
-  opts.on("-i", "--include-unstaged", "Include files with unstaged changes in addition to those with staged changes") {|i| options[:include_unstaged] = i}
-  opts.on("-c", "--check", "Don't actually modify files, just throw an error if incorrect formatting is found.") {|c| options[:check] = c}
 end
 
 # Parse arguments and deal with exceptions.
@@ -19,33 +14,74 @@ rescue OptionParser::InvalidOption, OptionParser::MissingArgument
   exit(2)
 end
 
-$line_pointer = 0
-$lines = []
+comment = '\/\/.*'
+
+$attributes_group = '\((.*?)\)'
+$name_and_type_group = '(.*)'
+$comment_group = '(\/?\/? .*)'
 
 def parse_line(line) 
   case line
-  when /@property/
-    return format_property(line)
+  when / ^ \s* @property \s* #{$attributes_group} \s* #{$name_and_type_group} ; #{$comment_group}/x
+    return format_property(line, Regexp.last_match)
+  when / ^ \s* [+-] \s* \( .+ \) \s* .+ /x
+    return format_method(line, Regexp.last_match)
   else
     return line
   end
 end
 
-def format_property(line)
+def compress_whitespace(string)
+  string.gsub(/\s+/, " ")
 end
+
+
+def format_property(line, match_data)
+  
+  property_attribute_order = ["atomic", "nonatomic", "strong", "weak", "assign", "copy", "readonly", "readwrite"]
+
+  # Extract and sort property attributes
+  attributes_array = match_data[1].split(",").collect do |a|
+    a.strip
+  end.sort do |a, b|
+    (property_attribute_order.index(a) || 9999) <=> (property_attribute_order.index(b) || 9999)
+  end
+  attributes_string = attributes_array.join(", ")
+
+  # Extract name and type
+  name_and_type_string = compress_whitespace match_data[2].strip
+  # Remove any spaces after asterisks
+  name_and_type_string.gsub! /\s* \* \s*/x, " *"
+
+  # Extract comment
+  comment_string = match_data[3].strip
+  
+  return "@property \(#{attributes_string}\) #{name_and_type_string}; #{comment_string}\n"
+end
+
+def format_method(line, match_data)
+  line = compress_whitespace line
+
+  # Strip space around parens, colon, and semicolon
+  line.gsub! /\s* ([():;]) \s*/x, '\1'
+  # Format plus or minus
+  # Whitespace between '+/-' and '(' will have just been removed.
+  line.gsub! /\s* [+-] \( /x, '- ('
+  # Fix space around asterisk
+  line.gsub! /([^*\s\t\r\n\f]) \s* \* \s*/x, '\1 *'
+  
+  return line
+end 
 
 input_file_name = unparsed.first
 output_file_name = unparsed.last
 
 output_file = File.new output_file_name, "w"
 
-
 File.open(input_file_name) do |file|
-  $lines = file.readlines
-  while $line_pointer < $lines.count
-    output_file.write parse_line $lines[$line_pointer]
-    $line_pointer += 1
+  file.each_line do |line|
+    new_line = parse_line line
+    output_file.write(new_line.rstrip + "\n") if new_line
   end
 end
 
-output_file.close
